3e728747c07def4dad0c0bfb7824296c317fe511
bisha-io <bisha.io.mail@gmail.com>
2021-04-05 18:59:24 +0200

    change settings.py

--- goldenarrow/settings.py
+++ goldenarrow/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "l4f89u1299#qgjqaanm#ov(&@c3@%8an=x@!fniqn6e+ax@qlr"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://goldenarrow.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["https://goldenarrow.herokuapp.com/", "127.0.0.1"]
 
 
 # Application definition
